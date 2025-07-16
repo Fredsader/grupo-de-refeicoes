a4dea29323e323d3449cd2575acc121821d957b7
Fred Sader <fredrangelsader@gmail.com>
2025-07-16 16:23:45 -0300

    commit-3-register-e-login-funcionando

--- server/index.js
+++ server/index.js
@@ -1,45 +1,20 @@
-const bcrypt = require('bcrypt');
-
 const express = require('express');
-const pool = require('./db');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-app.use(express.json);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
-    res.send('O coração do nosso site está batendo!');
-});
-
-app.post('/api/auth/register', (req, res) => {
-    const { name, email, password } = req.body;
-    const saltRounds = 10;
-
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-        if (err){
-            console.error('Erro ao gerar hash:', err);
-            return;
-        }
-
-        console.log('hash herado:', hash);
-    });
-
-    pool.query('INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3);', [name, email, password]);
-});
-
-app.post('/api/auth/login', (req, res) => {
-    const { email, password } = req.body;
-    try{   
-       const result = pool.query('SELECT FROM users WHERE email = $1', [email]);
-
-        bcrypt.compare(password, result.rows[0].password_hash);
-
-
-    } catch (err){
-        console.error(err);
-        res.status(500).json({erro: 'Usuário não encontrado'});
-    }
+  res.send('O coração do nosso site está batendo!');
 });
 
 app.listen(PORT, () => {
-    console.log(`O servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
---
+++ server/routes/auth.js
@@ -0,0 +1,63 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const express = require('express');
+const router = express.Router();
+const pool = require('../db');
+
+
+router.use(express.json());
+const PORT = 5000;
+
+
+router.post('/register', async (req, res) => {
+    try{
+        const { name, email, password } = req.body;
+        const saltRounds = 10;
+        
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        
+        if(user.rows.length > 0){
+            return res.status(409).json("Este email já está cadastrado.");
+        }
+
+        const password_hash = await bcrypt.hash(password, saltRounds);
+
+        const newUser = await pool.query('INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3);', [name, email, password_hash]);
+
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send("Erro no servidor")
+    }
+});
+
+router.post('/login', async (req, res) => {
+    console.log('Dados recebidos no corpo do /login:', req.body);
+    console.log(`Procurando por um usuário com o email: "[${req.body.email}]"`);
+   
+    try{   
+       const { email, password } = req.body;
+
+       const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+       if (result.rows.length === 0){
+        return res.status(401).json("Credencial inválido");
+       }
+
+       const user = result.rows[0];
+
+       const validPassword = await bcrypt.compare(password, user.password_hash);
+       if(!validPassword){
+        return res.status(401).json("Credencial inválido");
+       }
+
+       const payload = {userId: user.user_id};
+       const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1h'});
+
+       res.json({token});
+    } catch (err){
+        console.error(err);
+        res.status(500).json({erro: 'Usuário não encontrado'});
+    }
+});
+
+
+module.exports = router;
